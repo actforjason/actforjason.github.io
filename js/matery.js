373be92aac90da070f5556e21e12d10d3ad4e6be
actforjason <15236650610@163.com>
2021-05-20 21:06:03 +0800

    Site updated: 2021-05-20 21:06:03

--- js/matery.js
+++ js/matery.js
@@ -74,7 +74,8 @@ $(function () {
 
         $('#articleContent img').each(function () {
             let imgPath = $(this).attr('src');
-            $(this).wrap('<div class="img-item" data-src="' + imgPath + '" data-sub-html=".caption"></div>');
+            $(this).wrap('<div class="img-item" data-src="' + imgPath + '"></div>');
+            /*$(this).wrap('<div class="img-item" data-src="' + imgPath + '" data-sub-html=".caption"></div>');
             // 图片添加阴影
             $(this).addClass("img-shadow img-margin");
             // 图片添加字幕
@@ -89,7 +90,7 @@ $(function () {
             } else {
                 captionText = alt;
             }
-            // 字幕不空，添加之
+            // 字幕不空，添加之 captionText !== "", 修改
             if (captionText !== "") {
                 let captionDiv = document.createElement('div');
                 captionDiv.className = 'caption';
@@ -98,12 +99,12 @@ $(function () {
                 captionEle.innerText = captionText;
                 captionDiv.appendChild(captionEle);
                 this.insertAdjacentElement('afterend', captionDiv)
-            }
+            }*/
         });
         $('#articleContent, #myGallery').lightGallery({
             selector: '.img-item',
             // 启用字幕
-            subHtmlSelectorRelative: true
+            subHtmlSelectorRelative: false
         });
 
         // progress bar init
